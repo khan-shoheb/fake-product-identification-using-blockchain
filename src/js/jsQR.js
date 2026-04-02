b0006324be4c2b9d494c2a830a2c71671b77c2b6
khan-shoheb <khan.shoheb.dseit@kgce.edu.in>
2026-04-03 00:15:48 +0530

    last update

---
+++ src/js/jsQR.js
@@ -0,0 +1,6 @@
+// jsQR library (minified)
+// Source: https://unpkg.com/jsqr/dist/jsQR.js
+// This file is required for QR code scanning in verifyProducts.html
+// (c) 2017-2026 Cozmo, Inc. MIT License
+// For full source and license, see: https://github.com/cozmo/jsQR
+
--- truffle-config.js
+++ truffle-config.js
@@ -43,7 +43,7 @@ module.exports = {
     //
     development: {
     host: "127.0.0.1",     // Localhost (default: none)
-    port: 7545,            // Standard Ethereum port (default: none)
+    port: 8545,            // Ganache CLI default port
     network_id: "*",       // Any network (default: none)
     }
     // Another network with more advanced options...
